5c433a72ecef6e222048aefb6a1036b239334d9b
HamnYce <Hamadboa@gmail.com>
2021-01-14 12:16:25 +0300

    fixed file names, new solution

--- Replacewithalphabetposition/Replacewithalphabetposition.py
+++
@@ -1,5 +0,0 @@
-#create dictionary,
-#ooorr convert to unicode and find an algorithm that minus/pluses slash something to reach the
-# original sequence of the alphabet
-#probably some modulus 26 shit, check program for converting letters you did one time, should help.
-#converting text to lower case might help... might.
